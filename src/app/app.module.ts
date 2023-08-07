f3ae44095a7bd5150c65387799366b0b8f0e6cc7
Tarcisiosilvahive <tarcisio.silva@hivecloud.br>
2023-08-06 21:11:49 -0300

    configuração do bootstrap e primeiros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainModule } from './main/main.module';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './assignment/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './assignment/success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule,
-    MainModule
+    MainModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
